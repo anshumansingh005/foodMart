8ac71233612c021a6900eb8a8fbd360d3a4ea34d
Anshuman Singh <anshumansingh005@gmail.com>
2025-02-06 10:07:24 +0530

    added restaurant card initial design

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -1,6 +1,17 @@
-import React, { ReactElement } from "react";
+import React from "react";
 
-const RestaurantCard: React.FC = (): ReactElement => {
+interface RestaurantCardProps {
+  name: string;
+  cuisine: string;
+  rating: string;
+  deliveryTime: string;
+}
+const RestaurantCard: React.FC<RestaurantCardProps> = ({
+  name,
+  cuisine,
+  rating,
+  deliveryTime,
+}) => {
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
       <img
@@ -8,10 +19,10 @@ const RestaurantCard: React.FC = (): ReactElement => {
         alt="logo"
         src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/1/8/77219eab-c0f4-4d51-b93b-ba8d06457d91_1018574%20(1).jpg"
       />
-      <h3>Donne Biryani @99</h3>
-      <h4>Biryani, North Indian, Asian</h4>
-      <h4>4.4 ratings</h4>
-      <h4>34 Minutes</h4>
+      <h3>{name}</h3>
+      <h4>{cuisine}</h4>
+      <h4>{rating}</h4>
+      <h4>{deliveryTime}</h4>
     </div>
   );
 };
