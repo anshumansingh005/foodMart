4c46c4015c48bd02fd7151f1cb3718eb4ad3dea7
Anshuman Singh <anshumansingh005@gmail.com>
2025-02-06 17:36:59 +0530

    optimised code for better rendering

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -2,27 +2,28 @@ import React from "react";
 
 interface RestaurantCardProps {
   name: string;
-  cuisine: string;
-  rating: string;
-  deliveryTime: string;
+  cuisine: string[] | [];
+  imgId?: string;
+  avgRating: number | undefined;
+  deliveryTime: number;
 }
 const RestaurantCard: React.FC<RestaurantCardProps> = ({
   name,
   cuisine,
-  rating,
+  imgId,
+  avgRating,
   deliveryTime,
 }) => {
+  const baseUrl: string = `https://media-assets.swiggy.com/swiggy/image/upload/`;
+
+  const imgSrc = `${baseUrl}${imgId}`;
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
-      <img
-        className="res-logo"
-        alt="logo"
-        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/1/8/77219eab-c0f4-4d51-b93b-ba8d06457d91_1018574%20(1).jpg"
-      />
+      <img className="res-logo" alt="logo" src={imgSrc} />
       <h3>{name}</h3>
-      <h4>{cuisine}</h4>
-      <h4>{rating}</h4>
-      <h4>{deliveryTime}</h4>
+      <h4>{cuisine.join(", ")}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4>{deliveryTime} minutes</h4>
     </div>
   );
 };
