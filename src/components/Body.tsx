38dae58fb0806c7a8babedf1306494a8bd5b30d7
Anshuman Singh <anshumansingh005@gmail.com>
2025-02-09 21:10:48 +0530

    Added filter feature to filter out cards using hooks

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,18 +1,30 @@
-import React, { ReactElement } from "react";
+import React, { useState } from "react";
 import resConfig from "../utils/config";
 import RestaurantCard from "./RestaurantCard";
-export const Body: React.FC = (): ReactElement => {
+export const Body: React.FC = () => {
+  let [listOfRestaurants, setListOfRestaurants] = useState(resConfig);
   return (
     <div className="body">
-      <div className="search">Search bar</div>
+      <button
+        className="search"
+        onClick={() => {
+          listOfRestaurants = listOfRestaurants.filter(
+            (res) => res.info.avgRating !== undefined && res.info.avgRating > 4
+          );
+
+          setListOfRestaurants(listOfRestaurants);
+        }}
+      >
+        Filter
+      </button>
       <div className="res-container">
-        {resConfig.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard
             key={restaurant.info.id}
             name={restaurant.info.name}
             cuisine={restaurant.info.cuisines}
             deliveryTime={restaurant.info.sla.deliveryTime}
-            avgRating={restaurant.info.avgRating}
+            avgRating={restaurant.info.avgRating ?? 0}
             imgId={restaurant.info.cloudinaryImageId}
           />
         ))}
