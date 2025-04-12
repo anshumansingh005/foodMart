d89bd670c744a543566664e90051c4f42b990df1
anshumansingh005 <anshumansingh005@gmail.com>
2025-04-12 22:03:38 +0530

    Memoised function

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -7,24 +7,20 @@ interface RestaurantCardProps {
   avgRating: number | undefined;
   deliveryTime: number;
 }
-const RestaurantCard: React.FC<RestaurantCardProps> = ({
-  name,
-  cuisine,
-  imgId,
-  avgRating,
-  deliveryTime,
-}) => {
-  const baseUrl: string = `https://media-assets.swiggy.com/swiggy/image/upload/`;
+const RestaurantCard: React.FC<RestaurantCardProps> = React.memo(
+  ({ name, cuisine, imgId, avgRating, deliveryTime }) => {
+    const baseUrl: string = `https://media-assets.swiggy.com/swiggy/image/upload/`;
 
-  const imgSrc = `${baseUrl}${imgId}`;
-  return (
-    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
-      <img className="res-logo" alt="logo" src={imgSrc} />
-      <h3>{name}</h3>
-      <h4>{cuisine.join(", ")}</h4>
-      <h4>{avgRating} stars</h4>
-      <h4>{deliveryTime} minutes</h4>
-    </div>
-  );
-};
+    const imgSrc = `${baseUrl}${imgId}`;
+    return (
+      <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+        <img className="res-logo" alt="logo" src={imgSrc} />
+        <h3>{name}</h3>
+        <h4>{cuisine.join(", ")}</h4>
+        <h4>{avgRating} stars</h4>
+        <h4>{deliveryTime} minutes</h4>
+      </div>
+    );
+  }
+);
 export default RestaurantCard;
